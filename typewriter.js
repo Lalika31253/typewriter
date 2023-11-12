44c153873f5cdb4e36e29727dfe0fd892ead923f
Lalika31253 <Lalika31253@gmail.com>
2023-11-11 17:45:39 -0700

    Add a newline character at the end of the animation

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,13 @@
 const sentence = "hello there from lighthouse labs";
 
-function animatedSentence() {
+const animatedSentence = function () {
   let i = 0;
   const interval = setInterval(() => {
-    process.stdout.write(sentence[i++]);
+    process.stdout.write(sentence[i++] + '\n');
     if (i === sentence.length) {
       clearInterval(interval);
     }
-  }, 100)
+  }, 300)
 }
 
 animatedSentence();
